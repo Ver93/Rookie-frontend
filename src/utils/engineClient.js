8f12eb82906b78f037fbe76c99881d2cce14bb8f
Ver93 <VictorElvisRotman@outlook.com>
2026-07-16 13:07:56 +0200

    Fixed Small Issues

--- src/utils/engineClient.js
+++ src/utils/engineClient.js
@@ -3,20 +3,39 @@ import { uci, isready, go, position } from "../services/engine_api";
 
 export async function runEngine(depth, game) {
     const uciResp = await uci();
-    console.log(uciResp.body);
-    console.log(uciResp.error);
-    if (uciResp.error || uciResp.body !== "uciok") return null;
-    
+
+    console.log(uciResp);
+
+    if (!uciResp || uciResp.ans !== "uciok") {
+        return null;
+    }
+
+
     const readyResp = await isready();
-    if (readyResp.error || readyResp.body !== "isready") return null;
+
+    console.log(readyResp);
+
+    if (!readyResp || readyResp.ans !== "readyok") {
+        return null;
+    }
+
 
     const fen = game.fen();
-    const moves = game.history({ verbose: true }).map(m => m.from + m.to + (m.promotion || ""));
+
+    const moves = game.history({ verbose: true })
+        .map(m => m.from + m.to + (m.promotion || ""));
+
 
     console.log("position");
+
     const posResp = await position(fen, moves);
-    if (posResp?.error) return null;
+
+    if (!posResp || posResp.error) {
+        return null;
+    }
+
 
     const bestmove = await go(depth);
+
     return bestmove ?? null;
 }
