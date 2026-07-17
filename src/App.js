58b30b03d23fc09503f849803a1e4e3d0ace6cbb
Ver93 <VictorElvisRotman@outlook.com>
2026-07-17 08:01:37 +0200

    Added Sound

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import useEngine from "./hooks/useEngine";
 
 import { useState, useEffect } from "react";
 import { parseTimeControl } from "./hooks/useClock";
-import { loadSound } from "./utils/sound";
+import { loadSound, unlockAudio } from "./utils/sound";
 
 import styles from "./App.module.css";
 
