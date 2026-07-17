780d4ef14849aad124a57fdb0f6a773b662f2c62
Ver93 <VictorElvisRotman@outlook.com>
2026-07-17 07:52:18 +0200

    Added Sound

--- src/utils/sound.js
+++ src/utils/sound.js
@@ -1,48 +1,8 @@
-let audioContext;
-let moveBuffer;
-let loading = false;
+const audio = new Audio("/gui/sounds/move.mp3");
 
-export async function loadSound() {
-    if (moveBuffer || loading) return;
-
-    loading = true;
-
-    console.time("sound load");
-
-    audioContext ??= new AudioContext();
-
-    const response = await fetch("/gui/sounds/move.mp3");
-
-    console.log("status:", response.status);
-    console.log("size:", response.headers.get("content-length"));
-
-    const data = await response.arrayBuffer();
-
-    console.timeLog("sound load", "download done");
-
-    moveBuffer = await audioContext.decodeAudioData(data);
-
-    console.timeEnd("sound load");
-
-    loading = false;
-}
-
-export async function playMoveSound(enabled) {
+export function playMoveSound(enabled) {
     if (!enabled) return;
 
-    if (!moveBuffer) {
-        await loadSound();
-    }
-
-    if (audioContext.state === "suspended") {
-        await audioContext.resume();
-    }
-
-    const source = audioContext.createBufferSource();
-
-    source.buffer = moveBuffer;
-
-    source.connect(audioContext.destination);
-
-    source.start(0);
+    audio.currentTime = 0;
+    audio.play().catch(console.error);
 }
